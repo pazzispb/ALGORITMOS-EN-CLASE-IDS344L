be2015cf2e829dc08cf0f8905053c3549e37dad6
pazzispb <89502095+pazzispb@users.noreply.github.com>
2023-01-12 21:27:48 -0400

    Update FlorerosSalvador.cpp

--- Los floreros de Salvador/FlorerosSalvador.cpp
+++ Los floreros de Salvador/FlorerosSalvador.cpp
@@ -6,11 +6,11 @@ Autor: Pazzis Paulino ID: 1103790
 
 Asunto: Crear un programa que indique cuantos floreros le quedan a salvador por vender.
 
-Hace algún tiempo Salvador compro 100 floreros para
+Hace algÃºn tiempo Salvador compro 100 floreros para
 venderlos en un mercadillo. La semana pasada, despues de
-haber vendido algunos de ellos en los días anteriores, tenia los
+haber vendido algunos de ellos en los dÃ­as anteriores, tenia los
 floreros ordenados en 6 filas, con el mismo numero de unidades
-en cada una de ellas. En esta semana vendió 8 floreros y en la
+en cada una de ellas. En esta semana vendiÃ³ 8 floreros y en la
 actualidad tiene los que le quedan , ordenados en 11 filas, con el
 mismo numero de floreros en cada una.
 
@@ -27,12 +27,11 @@ int main(){
 			if((100 - (i + 8)) % 11 == 0){
 				contador++;
 				cout << "OPCION " << contador <<endl;
-				cout << "La semana pasada vendio " << i << " floreros" << endl;
-				cout << "Esta semana vendio " << i + 8 << " floreros" << endl << endl;
+				cout << "Salvador tiene  " << 100 - (i + 8) << " floreros" << endl;
+				cout << "Vendio " << (i+8) << endl << endl;
 			}	
 		}		
 	}
 	return 0;
 }
-
 
